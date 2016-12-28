50b5847a8d6a318b97ef247d87b0e6fafa85496f
maali <maali_hasan@hotmail.com>
2016-12-28 02:46:47 +0200

    download mongojs,start implemnt add user interface

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,18 +1,15 @@
 var app = angular.module('myApp', []);
-app.controller('appCtrl', function($scope) {
+app.controller('appCtrl', function($scope ,$http) {
     console.log("hello from controller");
-    person1={
-        name:'ahmad',
-        email:'ahmad@hotmail.com',
-        phone:'0597828105'
-    };
-    person2={
-        name:'maali',
-        email:'maali@hotmail.com',
-        phone:'0597828105'
-    };
 
-    var contactList=[person1,person2];
-    $scope.contactList=contactList;
+    $http.get('/usersList').then(function(response) {
+      console.log("  I recived json data i requested");
+      console.log(response.data);
+      $scope.usersList=response.data;
+
+
+    });
+
+
 });
 
--- server.js
+++ server.js
@@ -3,9 +3,11 @@
  * Created by maali hasan on 12/24/2016.
  */
 
-var express = require('express')
+var express = require('express');
 var fs= require('fs');
 var path = require('path');
+var mongojs= require('mongojs');
+var db= mongojs("usersList",['usersList'])
 
 var app = express();
 
@@ -15,7 +17,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/js',express.static(path.join(__dirname, '/js')));
 
-console.log(path.join(__dirname,'/public/controllers'));
+
 
 
 app.get('/index2.html', function (req, res) {
@@ -23,22 +25,24 @@ app.get('/index2.html', function (req, res) {
 
 });
 
-app.get('/childinfo.html', function (req, res) {
-    res.sendFile( __dirname + "/" + "childinfo.html" );
+app.get('/usersList', function (req, res) {
+  console.log("i recived a Get Req");
+    person1={
+        name:'ahmad',
+        email:'ahmad@hotmail.com',
+        phone:'0597828105'
+   };
+     person2={
+        name:'maali',
+         email:'maali@hotmail.com',
+        phone:'0597828105'
+     };
+     var usersList=[person1,person2];
 
-});
-// app.get('/process_get', function (req, res) {
-//     // Prepare output in JSON format
-//
-//     response = {
-//          first_name:req.body.first_name,
-//         last_name:req.body.last_name
-//     };
-//
-// //console.log(response);
-//     res.end(JSON.stringify(response));
-//    // console.log(req);
-// });
+     console.log( res.json(usersList));
+
+
+ });
 
 
 var server=app.listen(3000, function () {
@@ -47,27 +51,3 @@ var server=app.listen(3000, function () {
 
     console.log('Example app listening  at %s %s',host,port)
 });
-//
-//  const http= require('http');
-//  const fs= require('fs');
-//  const url =require('url');
-//
-//  http.createServer(function (request,respone) {
-//      var pathname= url.parse(request.url).pathname;
-//      pathname=pathname.substr(1);
-//      console.log("request for "+pathname+"recived!"+"\n");
-//       fs.readFile(pathname,function (err,data) {
-//          if(err ){
-//              respone.writeHead(404,{'Content-Type': 'text/html'})
-//              return err.error();
-//          }
-//           respone.writeHead(200,{'Content-Type': 'text/html'})
-//           respone.write(data.toString());
-//          respone.end();
-//
-//
-//      })
-//
-//  }).listen(3000);
-// console.log('Server running at http://127.0.0.1:3000/');
-//
